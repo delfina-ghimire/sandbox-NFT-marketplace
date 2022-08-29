bb8795e54b88aceba35c1c4eda2729a59114b54b
delfina <clair.daisies@gmail.com>
2022-08-29 17:05:16 +0545

    Get started button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { Navbar } from './components';
+import { Hero, Navbar } from './components';
 import styles from './style';
 
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}><Navbar/></div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Hero</div>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Billing
-        CardDeal
-        Testimonials
-        Clients
-        Click to Action
+        Stats Business Billing CardDeal Testimonials Clients Click to Action
         Footer
-        </div>
+      </div>
     </div>
   </div>
 );
